631468ec2283c35bb9aa5fdf181c47ad4e89c284
ykrods <890082+ykrods@users.noreply.github.com>
2014-08-31 18:41:37 +0900

    add memo script

---
+++ init.el.d/memo.init.el
@@ -0,0 +1,44 @@
+;=====================
+; web storage notebook
+;=====================
+(setq notebook-directory "~/Copy/text/note")
+
+(defun web-storage-notebook-open-new-file ()
+  "open a new notebook"
+  (interactive)
+  (when (not (file-directory-p
+              (concat notebook-directory
+                      (format-time-string "/%Y" (current-time)) )))
+    (make-directory (concat notebook-directory (format-time-string "/%Y"))) )
+
+
+  (when (not (file-directory-p (concat notebook-directory
+                                       (format-time-string "/%Y/%m"))))
+    (make-directory (concat notebook-directory (format-time-string "/%Y/%m"))))
+  (when (not (file-directory-p (concat notebook-directory
+                                       (format-time-string "/%Y/%m/%d"))))
+    (make-directory (concat notebook-directory (format-time-string "/%Y/%m/%d"))))
+
+  (find-file (concat notebook-directory (format-time-string "/%Y/%m/%d/%H%M%S") ".txt")))
+
+(global-set-key (kbd "C-c m") 'web-storage-notebook-open-new-file)
+
+(defun web-storage-notebook-open-notebook-dir ()
+  "open the notebook dir"
+  (interactive)
+  (dired notebook-directory))
+
+(defun web-storage-search()
+  "search words in notebook"
+  (interactive)
+  (grep-find
+    (format "ag --nocolor --nogroup -- %s %s"
+            (read-from-minibuffer "words: "
+                                  ""
+                                  nil
+                                  nil
+                                  'grep-find-history)
+            notebook-directory)
+    ))
+
+(global-set-key (kbd "C-c M") 'web-storage-search)

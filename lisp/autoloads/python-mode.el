f110cb87e918f557093cc23eba95aaa85b14b1cc
ykrods <890082+ykrods@users.noreply.github.com>
2017-08-26 18:00:19 +0900

    append project path to jedi sys-path option

--- lisp/autoloads/python-mode.el
+++ lisp/autoloads/python-mode.el
@@ -1,3 +1,17 @@
+;; inspired
+;; - https://gist.github.com/nyergler/6100112
+;; - https://github.com/bbatsov/projectile/blob/master/projectile.el
+(defun parent-dir (path)
+  (directory-file-name (file-name-directory (directory-file-name (expand-file-name path)))))
+
+(defun search-project-dir (path)
+  (if (equal path "/")
+      nil
+    (if (file-exists-p (expand-file-name ".git" (parent-dir path)))
+        (parent-dir path)
+      (search-project-dir (parent-dir path)))))
+
+
 (defun run-as-script ()
   (interactive)
   (shell-command buffer-file-name))
@@ -16,10 +30,16 @@
 
   (add-hook 'python-mode-hook
             '(lambda()
-               (setq jedi:server-args
-                     '("--virtual-env" "~/.emacs.d/.python-environments/completion"))
+               (make-local-variable 'jedi:server-args)
+               (set 'jedi:server-args
+                     (list "--virtual-env" "~/.emacs.d/.python-environments/completion"))
+
+               (make-local-variable 'project-root)
+               (set 'project-dir (search-project-dir (buffer-file-name)))
+               (when project-dir
+                 (set 'jedi:server-args (append jedi:server-args (list "--sys-path" project-dir)))
+                 )
                (jedi:setup)
                (add-to-list 'company-backends 'company-jedi) ; backendに追加
                ))
-
 )

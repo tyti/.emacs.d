a32137e386d87ec76aa579dc3c0edaaedfe93647
ykrods <890082+ykrods@users.noreply.github.com>
2017-12-17 15:35:26 +0900

    add dired.el

---
+++ lisp/autoloads/dired.el
@@ -0,0 +1,13 @@
+(defun my/dired-open-file ()
+  "In dired, open the file named on this line."
+  (interactive)
+  (let* ((file (dired-get-filename)))
+    (message "Opening %s..." file)
+    (call-process "open" nil 0 nil file) ))
+
+(add-hook 'dired-mode-hook
+  (lambda ()
+    (define-key dired-mode-map (kbd "M-o") 'my/dired-open-file)
+    (define-key dired-mode-map (kbd "^")
+      (lambda () (interactive) (find-alternate-file "..")))
+  ))

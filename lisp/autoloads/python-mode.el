c7dce9ba1b2ccb5754af6b63c8c12be5a615f8c4
ykrods <890082+ykrods@users.noreply.github.com>
2018-09-17 19:15:54 +0900

    remove jedi

--- lisp/autoloads/python-mode.el
+++ lisp/autoloads/python-mode.el
@@ -9,30 +9,4 @@
              ;; enable flake8
              (flycheck-mode t)
              (local-set-key (kbd "C-c c") 'run-as-script)
-             )
-          )
-
-(when (require 'jedi-core nil t)
-  (setq jedi:complete-on-dot t)
-  (setq jedi:use-shortcuts t)
-
-  (add-hook 'python-mode-hook
-            '(lambda()
-               ;; sphinx-doc
-               (require 'sphinx-doc)
-               (sphinx-doc-mode t)
-
-               ;; jedi
-               (make-local-variable 'jedi:server-args)
-               (set 'jedi:server-args
-                     (list "--virtual-env" "~/.emacs.d/.python-environments/completion"))
-
-               (make-local-variable 'project-root)
-               (set 'project-dir (vc-find-root (buffer-file-name) ".git"))
-               (when project-dir
-                 (set 'jedi:server-args (append jedi:server-args (list "--sys-path" project-dir)))
-                 )
-               (jedi:setup)
-               (add-to-list 'company-backends 'company-jedi) ; backendに追加
-               ))
-)
+             ))

b1a323af9c1c4d3792fa3e6244b760b4a9ffee80
ykrods <890082+ykrods@users.noreply.github.com>
2018-11-25 13:58:35 +0900

    python mode

--- lisp/autoloads/python-mode.el
+++ lisp/autoloads/python-mode.el
@@ -1,12 +1,4 @@
-
-
-(defun run-as-script ()
-  (interactive)
-  (shell-command buffer-file-name))
-
 (add-hook 'python-mode-hook
           '(lambda ()
-             ;; enable flake8
-             (flycheck-mode t)
-             (local-set-key (kbd "C-c c") 'run-as-script)
+             ;;
              ))

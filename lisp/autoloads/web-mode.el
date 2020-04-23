de0191dca8d4b37c9a3d2a235885e1d99dba1a79
ykrods <890082+ykrods@users.noreply.github.com>
2020-04-23 18:55:53 +0900

    apply web-mode to .vue

--- lisp/autoloads/web-mode.el
+++ lisp/autoloads/web-mode.el
@@ -1,6 +1,6 @@
 
 (when (require 'web-mode nil t)
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.\\(html\\|vue\\)\\'" . web-mode))
   ; (setq web-mode-enable-auto-closing t)
   (setq web-mode-enable-auto-pairing t)
 )

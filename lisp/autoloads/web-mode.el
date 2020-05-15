1dda970c6e210f1bc274447069bef39304511732
ykrods <890082+ykrods@users.noreply.github.com>
2020-05-15 09:06:12 +0900

    Apply web-mode to .svelte

--- lisp/autoloads/web-mode.el
+++ lisp/autoloads/web-mode.el
@@ -1,6 +1,6 @@
 
 (when (require 'web-mode nil t)
-  (add-to-list 'auto-mode-alist '("\\.\\(html\\|vue\\)\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.\\(html\\|vue\\|svelte\\)\\'" . web-mode))
   ; (setq web-mode-enable-auto-closing t)
   (setq web-mode-enable-auto-pairing t)
 )

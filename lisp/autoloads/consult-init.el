fc86851cff74b34dded441d887dd5e326e288b4a
ykrods <890082+ykrods@users.noreply.github.com>
2023-12-24 19:16:17 +0900

    Replace consult-ag to consult-repgrep (fix)

--- lisp/autoloads/consult-init.el
+++ lisp/autoloads/consult-init.el
@@ -3,8 +3,5 @@
 (when (require 'consult nil t)
   (global-unset-key (kbd "C-x C-b"))
   (global-set-key (kbd "C-x C-b") 'consult-buffer)
-
-  (when (require 'consult-ag nil t)
-    (global-set-key (kbd "C-c f") 'consult-ripgrep)
-    )
+  (global-set-key (kbd "C-c f") 'consult-ripgrep)
   )

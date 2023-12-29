cc9aa5ad78a945f0e52d8926e5785998dfe6fc9c
ykrods <890082+ykrods@users.noreply.github.com>
2023-12-29 21:47:41 +0900

    fix recentf-exclude

--- lisp/custom.el
+++ lisp/custom.el
@@ -18,7 +18,7 @@
    '(rg consult vertico markdown-mode eglot yaml-mode web-mode projectile magit hydra editorconfig docker csv-mode company ##))
  '(python-indent-guess-indent-offset-verbose nil)
  '(recentf-exclude
-   '("/\\(\\(\\(COMMIT\\|NOTES\\|PULLREQ\\|MERGEREQ\\|TAG\\)_EDIT\\|MERGE_\\|\\)MSG\\|\\(BRANCH\\|EDIT\\)_DESCRIPTION\\)\\'" "\\\\.gpg"))
+   '("/\\(\\(\\(COMMIT\\|NOTES\\|PULLREQ\\|MERGEREQ\\|TAG\\)_EDIT\\|MERGE_\\|\\)MSG\\|\\(BRANCH\\|EDIT\\)_DESCRIPTION\\)\\'" ".+\\.gpg"))
  '(recentf-max-saved-items 50)
  '(recentf-mode t)
  '(size-indication-mode t)

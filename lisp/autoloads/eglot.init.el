1bc01ab59fca4490cdf2474ac8adf590c6a2330c
ykrods <890082+ykrods@users.noreply.github.com>
2025-01-21 17:25:03 +0900

    Eglot typescript

--- lisp/autoloads/eglot.init.el
+++ lisp/autoloads/eglot.init.el
@@ -2,11 +2,10 @@
 (when (require 'eglot nil t)
   (add-to-list 'eglot-server-programs
                '(python-mode . ("localhost" 12000))
+               ;; npm install -g typescript-language-server
+               '((js-mode typescript-mode) . ("typescript-language-server" "--stdio"))
                )
 
-  ;; npm install -g typescript-language-server
-  (add-to-list 'eglot-server-programs
-               '(js-mode . ("typescript-language-server" "--stdio"))
-               )
   (add-hook 'js-mode-hook 'eglot-ensure)
+  (add-hook 'typescript-mode-hook 'eglot-ensure)
   )
